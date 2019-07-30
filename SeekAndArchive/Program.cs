bc7763080ba14748e9eb28659f9bfd0ac58cf839
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-07-30 13:25:24 +0200

    Task completed

--- SeekAndArchive/Program.cs
+++ SeekAndArchive/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SeekAndArchive
 {
     class Program
     {
+        private static List<FileInfo> foundFiles = new List<FileInfo>();
+
+        private void FileSearcher(DirectoryInfo parent, string fileName)
+        {
+            DirectoryInfo[] dis = parent.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                foreach (FileInfo fi in di.GetFiles())
+                {
+                    if (fileName == fi.Name)
+                    {
+                        foundFiles.Add(fi);
+                    }
+                }
+                FileSearcher(di, fileName);
+            }
+        }
+
         static void Main(string[] args)
         {
+            string fileName = args[0];
+            string directory = args[1];
+
+            DirectoryInfo parent = new DirectoryInfo(directory);
+            Program p = new Program();
+            p.FileSearcher(parent, fileName);
+            
+            foreach (var file in foundFiles)
+            {
+                Console.WriteLine(file.FullName);
+            }
+            Console.ReadLine();
         }
+
     }
 }
