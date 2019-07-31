22bdb361a707e0b1cc46a1458174365000a5f908
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-07-31 11:19:23 +0200

    Search function working

--- SeekAndArchive/Program.cs
+++ SeekAndArchive/Program.cs
@@ -1,52 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace SeekAndArchive
 {
     class Program
     {
-        private static List<FileInfo> foundFiles = new List<FileInfo>();
+        static List<FileInfo> foundFiles;
+        static List<FileSystemWatcher> watchers;
 
-        private void FileSearcher(DirectoryInfo parent, string fileName)
+        static void RecursiveSearch(List<FileInfo> foundFiles, DirectoryInfo currentDirectory, string fileName)
         {
-            DirectoryInfo[] dis = parent.GetDirectories();
-            foreach (DirectoryInfo di in dis)
+            Console.WriteLine(currentDirectory.FullName);
+            foreach (FileInfo fi in currentDirectory.GetFiles())
             {
-                foreach (FileInfo fi in di.GetFiles())
+                if (Regex.IsMatch(fi.Name, fileName))
                 {
-                    Match m = Regex.Match(fi.Name, fileName);
-                    if (m.Success)
-                    {
-                        foundFiles.Add(fi);
-                    }
+                    foundFiles.Add(fi);
                 }
-                FileSearcher(di, fileName);
+            }
+
+            foreach (DirectoryInfo dir in currentDirectory.GetDirectories())
+            {
+                RecursiveSearch(foundFiles, dir, fileName);
+            }
+        }
+
+        static void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            FileSystemWatcher senderWatcher = (FileSystemWatcher)sender;
+
+            if (e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                Console.WriteLine("{0} has been changed!", e.FullPath);
+            }
+            else if (e.ChangeType == WatcherChangeTypes.Renamed)
+            {
+                Console.WriteLine("{0} has been renamed!", e.FullPath);
+            }
+            else if (e.ChangeType == WatcherChangeTypes.Deleted)
+            {
+                Console.WriteLine("{0} has been deleted!", e.FullPath);
+                watchers.Remove(senderWatcher);
             }
         }
 
         static void Main(string[] args)
         {
+            foundFiles = new List<FileInfo>();
+            watchers = new List<FileSystemWatcher>();
             string fileName = args[0];
-            string directory = args[1];
+            string directory = Path.GetFullPath(args[1]);
 
             DirectoryInfo parent = new DirectoryInfo(directory);
-            Program p = new Program();
-            p.FileSearcher(parent, fileName);
+
 
             if (!parent.Exists)
             {
                 Console.WriteLine("The specified directory does not exist.");
+                Console.ReadLine();
                 return;
             }
-      
-            foreach (var file in foundFiles)
-            {
-                Console.WriteLine(file.FullName);
-            }
+
+            RecursiveSearch(foundFiles, parent, fileName);
+            Console.WriteLine("Found {0} files.", foundFiles.Count);
+
+
+            
             Console.ReadLine();
         }
-
     }
 }
